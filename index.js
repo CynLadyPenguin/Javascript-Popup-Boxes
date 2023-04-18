e4d4ffc071dbe56060f4de2c4dd377dee62701c8
Cynthia <ladypenguin1991@gmail.com>
2023-04-18 14:25:41 -0500

    finally finished

--- index.js
+++ index.js
@@ -2,13 +2,3 @@ const getThatName = ()  => {
   prompt('who are you again?');
 };
 getThatName();
-
-const changeWallpaper = () => {
-  $('.button').on('click', function(){
-  let confirm = confirm('Call the Power Rangers?');
-  if(confirm === true) {
-    $('body').css('background-image', 'url(images/Image20230418104213.png)');
-  }
-});
-};
-changeWallpaper();
